c9fcbdbaf3d33e81c034e6826fb42a9883534cea
Bashir Amokomowo <barsheyr@gmail.com>
2025-01-15 21:56:30 +0100

    fixed folder structure

--- src/App.js
+++ src/App.js
@@ -4,36 +4,24 @@ import Structure from "./pages/pageStructure";
 import SignIn from "./pages/auth pages/signin";
 import CreateAccount from "./pages/auth pages/createAccount";
 import ResetPassword from "./pages/auth pages/resetPassword";
-import Home from "./pages/general pages/homePage";
-import TailorProfile from "./pages/general pages/tailorProfile";
 import ErrorPage from "./pages/errorPage";
-import ProductDetail from "./pages/general pages/productPage";
-import Cart from "./pages/general pages/cart";
-import UserAccount from "./pages/userAccount/userAccount";
-import NewAccount from "./pages/userAccount/newAccount";
-import AccountDetails from "./pages/userAccount/AccountDetails";
-import SavedItems from "./pages/userAccount/savedItems";
-import ChangePassword from "./pages/userAccount/changePassword";
-import Measurements from "./pages/userAccount/Measurements";
-import Overview from "./pages/userAccount/Overview";
-import MessageCenter from "./pages/userAccount/MessageCenter";
-import Order from "./pages/userAccount/Order";
-import SavedStyles from "./pages/userAccount/SavedStyles";
-import ShippingAddress from "./pages/userAccount/ShippingAddress";
-import Feedback from "./pages/userAccount/Feedback";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/*" element={<Structure />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<CreateAccount />} />
-          <Route path="/forgot-password" element={<ResetPassword />} />
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-      </BrowserRouter>
+      <>
+        <ToastContainer bodyClassName={() => "flex items-center text-xs "} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/*" element={<Structure />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<CreateAccount />} />
+            <Route path="/forgot-password" element={<ResetPassword />} />
+            <Route path="*" element={<ErrorPage />} />
+          </Routes>
+        </BrowserRouter>
+      </>
     </div>
   );
 }
