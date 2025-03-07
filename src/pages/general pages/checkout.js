197eb486630e4f623a3231129372f7f1036f5db3
tobi <tobiamusa94@gmail.com>
2025-03-07 16:52:11 +0100

    weekend push

--- src/pages/general pages/checkout.js
+++ src/pages/general pages/checkout.js
@@ -6,24 +6,45 @@ import SelectInput from "../../components/select";
 import * as Yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import { placeOrder } from "../../hooks/local/reducer";
+import { useEffect, useState } from "react";
 
 const Checkout = () => {
   const { cart } = useCart();
   const dispatch = useDispatch();
 
   const userSessionData = useSelector((state) => state.user.userSession);
-  console.log(userSessionData);
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  useEffect(() => {
+    setIsSignedIn(!!userSessionData);
+  }, [userSessionData]);
+
+  const totalCost = cart.reduce((sum, item) => 
+    sum + ((parseFloat(item.cost.toString().replace(/,/g, "")) || 0) * (Number(item.quantity) || 1)), 
+    0
+  );
+  
+  const formattedTotalCost = totalCost.toLocaleString("en-US", { minimumFractionDigits: 2 });
+  
+
+  // const customerID = userSessionData
+  const phone = userSessionData?.data?.customerData?.phoneNumber || "";
+  const email = userSessionData?.data?.customerData?.emailAddress || "";
+  const fullName = userSessionData?.data?.customerData?.fullName || "";
+  const [firstName, ...lastNameParts] = fullName.split(" ");
+  const lastName = lastNameParts.join(" "); 
+
 
   const createOrder = useFormik({
           initialValues: {
-            is_signed_in: "",
+            is_signed_in: isSignedIn,
             customer_id: "",
             create_account: "",
-            email_address: "",
-            first_name: "",
-            last_name: "",
-            phone_number: "",
-            total_amount: "",
+            email_address: email,
+            first_name: firstName,
+            last_name: lastName,
+            phone_number: phone,
+            total_amount: formattedTotalCost,
             delivery_address: "",
             order: [],
           },
@@ -43,15 +64,6 @@ const Checkout = () => {
           },
         });
 
-  
-  const totalCost = cart.reduce((sum, item) => 
-    sum + ((parseFloat(item.cost.toString().replace(/,/g, "")) || 0) * (Number(item.quantity) || 1)), 
-    0
-  );
-  
-  const formattedTotalCost = totalCost.toLocaleString("en-US", { minimumFractionDigits: 2 });
-  
-
   console.log(cart);
     const serviceType = [
         { value: "express", label: "Express service" },
@@ -70,6 +82,13 @@ const Checkout = () => {
                     <Input label={"Shipping address:"} />
                     <Input label={"Full name:"} />
                     <Input label={"Phone number:"} />
+                  </div>
+                  <div>
+                    <div>Would you like us to create an account for you? <span>Creating a tailorlynk account allows you to conviniently keep track of your orders</span></div>
+                    <div className="text-xs border rounded w-fit mt-4 flex p-[2px]">
+                      <div className="rounded py-2 px-4 bg-primary text-white">Yes</div>
+                      <div className="rounded py-2 px-4 text-primary">No</div>
+                    </div>
                   </div>
                 </div>
             </div>
